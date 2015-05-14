34b22a8f68ef20e6e05d6a7e5929bcd5e6f10548
yuuho.tsuboi <yuuho.tsuboi@adish.com>
2015-05-14 10:01:05 +0900

    emacsにscss-modeを導入

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -172,15 +172,41 @@
 
   (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
 
-;; CSS mode
-(autoload 'css-mode "css-mode")
-(setq auto-mode-alist
-        (cons '("\\.css\\'" . css-mode) auto-mode-alist))
+;; CSS
+(defun my-css-electric-pair-brace ()
+  (interactive)
+  (insert "{")(newline-and-indent)
+  (newline-and-indent)
+  (insert "}")
+  (indent-for-tab-command)
+  (previous-line)(indent-for-tab-command)
+  )
 
-(setq cssm-indent-level 4)
-(setq cssm-indent-function #'cssm-c-style-indenter)
-(put 'dired-find-alternate-file 'disabled nil)
+(defun my-semicolon-ret ()
+  (interactive)
+  (insert ";")
+  (newline-and-indent))
 
+;; scss-mode
+;; https://github.com/antonj/scss-mode
+(autoload 'scss-mode "scss-mode" nil t)
+; (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(scss\\|css\\)\\'" . scss-mode))
+;(add-hook 'scss-mode-hook 'ac-css-mode-setup)
+(add-hook 'scss-mode-hook
+          (lambda ()
+            (define-key scss-mode-map "\M-[" 'my-css-electric-pair-brace)
+            ;;(define-key scss-mode-map ";" 'my-semicolon-ret)
+            (setq css-indent-offset 4)
+            (setq scss-compile-at-save nil)
+            (setq ac-sources '(ac-source-yasnippet
+                               ;; ac-source-words-in-same-mode-buffers
+                               ac-source-words-in-all-buffer
+                               ac-source-dictionary
+                               ))
+            (flymake-mode t)
+            ))
+;(add-to-list 'ac-modes 'scss-mode)
 
 ;; cperl-mode
 (add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
