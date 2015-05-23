8001688797177562951fc3e63cd0d05039e07a51
yuuho.tsuboi <yuuho.tsuboi@adish.co.jp>
2015-05-23 12:10:11 +0900

    Add web-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,9 +25,10 @@
 
 ;;; 日本語環境用
 ;(setq auto-coding-functions nil)
-(set-default-coding-systems 'utf-8)
 (set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
+(set-file-name-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-clipboard-coding-system 'utf-8)
@@ -38,6 +39,9 @@
 
 ;;; C-x:で指定行数にジャンプ
 (global-set-key "\C-x:" 'goto-line)
+
+;;; 行数を表示
+;(global-linum-mode t)
 
 ;;; 起動時の画面を表示しない
 (setq inhibit-splash-screen t)
@@ -127,50 +131,122 @@
 
 
 ;;; PHPモードの設定
-(require 'php-mode)
+;(require 'php-mode)
 ;;;php-modeの時は文字コードをUTF-8にする
 ;(custom-set-variables '(php-mode-force-pear t))
 ;(add-hook 'php-mode-hook (defun php-mode-hook () (c-set-style "bsd")))
-(add-hook 'php-mode-hook
-          (lambda ()
-            (c-set-offset 'case-label' c-basic-offset)
-            (c-set-offset 'arglist-intro' c-basic-offset)
-            ;(c-set-offset 'arglist-cont-nonempty' 4)
-            (c-set-offset 'arglist-close' 0)
-            (set-default-coding-systems 'utf-8)
-            (set-buffer-file-coding-system  'utf-8)
-            (set-terminal-coding-system 'utf-8)
-            ))
+;; (add-hook 'php-mode-hook
+;;           (lambda ()
+;;             (c-set-offset 'case-label' c-basic-offset)
+;;             (c-set-offset 'arglist-intro' c-basic-offset)
+;;             ;(c-set-offset 'arglist-cont-nonempty' 4)
+;;             (c-set-offset 'arglist-close' 0)
+;;             (set-default-coding-systems 'utf-8)
+;;             (set-buffer-file-coding-system  'utf-8)
+;;             (set-terminal-coding-system 'utf-8)
+;;             ))
 
-;;; nXhtml
-;(load "nxhtml/autostart.el")
-;;; mode背景
-;(custom-set-faces
-; '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background dark)) (:background "black"))))
-; '(mumamo-background-chunk-submode1 ((((class color) (min-colors 88) (background dark)) (:background "grey10"))))
-;)
+;;; web-mode
+(require 'web-mode)
+;; 拡張子の設定
+(add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tt2$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tx$"        . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js[x]?$"    . web-mode))
 
+;; (defadvice web-mode-highlight-part (around tweak-jsx activate)
+;;   (if (equal web-mode-content-type "jsx")
+;;       (let ((web-mode-enable-part-face nil))
+;;         ad-do-it)
+;;         ad-do-it))
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  ;; html indent
+  (setq web-mode-markup-indent-offset 2)
+  ;; css indent
+  (setq web-mode-css-indent-offset 2)
+  ;; script indent(js,php,etc..)
+  (setq web-mode-code-indent-offset 4)
+  (setq web-mode-content-types-alist
+        '(("jsx"  . "\\.js[x]?\\'")))
+  ;; htmlの内容をインデント
+  ;; TEXTAREA等の中身をインデントすると副作用が起こったりするので
+  ;; デフォルトではインデントしない
+  ;;(setq web-mode-indent-style 2)
+  ;; コメントのスタイル
+  ;;   1:htmlのコメントスタイル(default)
+  ;;   2:テンプレートエンジンのコメントスタイル
+  ;;      (Ex. {# django comment #},{* smarty comment *},{{-- blade comment --}})
+  (setq web-mode-comment-style 2)
+  ;; 終了タグの自動補完をしない
+  ;;(setq web-mode-disable-auto-pairing t)
+  ;; ;; color:#ff0000;等とした場合に指定した色をbgに表示しない
+  ;; (setq web-mode-disable-css-colorization t)
+  ;; ;;css,js,php,etc..の範囲をbg色で表示
+  ;; (setq web-mode-enable-block-faces t)
+  ;; (custom-set-faces
+  ;;  '(web-mode-server-face
+  ;;    ((t (:background "grey"))))                  ; template Blockの背景色
+  ;;  '(web-mode-css-face
+  ;;    ((t (:background "grey18"))))                ; CSS Blockの背景色
+  ;;  '(web-mode-javascript-face
+  ;;    ((t (:background "red"))))                ; javascript Blockの背景色
+  ;;  )
+  (setq web-mode-enable-heredoc-fontification t)
+  )
+(add-hook 'web-mode-hook  'web-mode-hook)
+
+;;色の設定
+(custom-set-faces
+ '(web-mode-html-tag-bracket-face
+   ((t (:foreground "gray60"))))
+ '(web-mode-doctype-face
+   ((t (:foreground "#82AE46"))))
+ '(web-mode-html-tag-face
+   ((t (:foreground "lightskyblue" :weight bold))))
+ '(web-mode-html-attr-name-face
+   ((t (:foreground "lightgoldenrod"))))
+ '(web-mode-html-attr-value-face
+   ((t (:foreground "lightsalmon"))))
+ '(web-mode-comment-face
+   ((t (:foreground "orangered"))))
+ '(web-mode-server-comment-face
+   ((t (:foreground "#D9333F"))))
+ '(web-mode-css-rule-face
+   ((t (:foreground "#A0D8EF"))))
+ '(web-mode-css-pseudo-class-face
+   ((t (:foreground "#FF7F00"))))
+ '(web-mode-css-at-rule-face
+   ((t (:foreground "#FF7F00"))))
+ )
 
 ;;; js2-mode
 
-(when (load "js2" t)
-  (setq js2-cleanup-whitespace nil
-	js2-mirror-mode nil
-	js2-bounce-indent-flag nil)
+;; (when (load "js2" t)
+;;   (setq js2-cleanup-whitespace nil
+;; 	js2-mirror-mode nil
+;; 	js2-bounce-indent-flag nil)
 
-  (defun indent-and-back-to-indentation ()
-    (interactive)
-    (indent-for-tab-command)
-    (let ((point-of-indentation
-	   (save-excursion
-	     (back-to-indentation)
-	     (point))))
-      (skip-chars-forward "\s " point-of-indentation)))
-  (define-key js2-mode-map "\C-i" 'indent-and-back-to-indentation)
+;;   (defun indent-and-back-to-indentation ()
+;;     (interactive)
+;;     (indent-for-tab-command)
+;;     (let ((point-of-indentation
+;; 	   (save-excursion
+;; 	     (back-to-indentation)
+;; 	     (point))))
+;;       (skip-chars-forward "\s " point-of-indentation)))
+;;   (define-key js2-mode-map "\C-i" 'indent-and-back-to-indentation)
 
-  (define-key js2-mode-map "\C-m" nil)
+;;   (define-key js2-mode-map "\C-m" nil)
 
-  (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
+;;   ;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+;;   )
 
 ;; CSS
 (defun my-css-electric-pair-brace ()
@@ -230,9 +306,9 @@
              ))
 
 ;; kolon-mode
-(autoload 'kolon-mode "kolon-mode")
-(add-to-list 'auto-mode-alist '("\\.tx$" . kolon-mode))
-(add-to-list 'auto-mode-alist '("\\.tt2$" . kolon-mode))
+;; (autoload 'kolon-mode "kolon-mode")
+;; (add-to-list 'auto-mode-alist '("\\.tx$" . kolon-mode))
+;; (add-to-list 'auto-mode-alist '("\\.tt2$" . kolon-mode))
 
 ;; yaml-mode
 (require 'yaml-mode)
