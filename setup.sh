cc83ccfd64167da1523e7b2f672974fefc6273c5
yuuho.tsuboi <yuuho.tsuboi@adish.co.jp>
2015-09-13 21:56:43 +0900

    起動ファイル修正

--- setup.sh
+++ setup.sh
@@ -5,18 +5,18 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 dir=$(cd $(dirname $0);pwd)
 
 # リンクを作成しないファイル名のリスト
-ignorefiles=("." ".." ".zshrc.local" ".git" ".gitsubmodule" ".gitignore" "${0##*/}")
+ignorefiles=("." ".." ".zshrc.local" ".git" ".gitmodules" ".gitignore" "${0##*/}")
 
-for dotfile in .?*; do
+for dotfile in ${dir}/.?*; do
   is_ignored=0
   for ignorefile in ${ignorefiles[@]}; do
-    if [ $dotfile = $ignorefile ]; then
+    if [ $dotfile = "${dir}/${ignorefile}" ]; then
       is_ignored=1
       break
     fi
   done
 
   if [ $is_ignored -ne 1 ]; then
-    ln -s -v -f "$dir/$dotfile" $HOME
+    ln -s -v -f $dotfile $HOME
   fi
 done
